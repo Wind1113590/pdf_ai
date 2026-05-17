f6ade85e1c1f072274da98406e669493e5d49eb4
Hong <h19359265517@qq.com>
2026-05-17 19:54:44 +0800

    项目初始化

---
+++ src/main/java/com/huang/pdf_ai/PdfAi2026517Application.java
@@ -0,0 +1,13 @@
+package com.huang.pdf_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfAi2026517Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfAi2026517Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/huang/pdf_ai/PdfAi2026517ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.pdf_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfAi2026517ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
